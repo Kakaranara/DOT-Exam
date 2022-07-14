94f93f37fed38804bfce8f198a30f6d81a495dea
kakaranara <wahyukoco535@gmail.com>
2022-07-14 23:56:21 +0700

    dialog in progress

--- lib/screen/add_expense.dart
+++ lib/screen/add_expense.dart
@@ -27,18 +27,50 @@ class _AddExpensePageState extends State<AddExpensePage> {
         child: ListView(
           children: [
             _input(controller: controller),
-            _listtile(),
+            _listtile(onTap: () {
+              show(context);
+            }),
           ],
         ),
       ),
     );
   }
 
-  Widget _listtile() {
+  void show(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(14), topRight: Radius.circular(14))),
+        builder: (_) {
+          return Container(
+            padding: const EdgeInsets.all(16),
+            child: ListView(
+              children: [
+                Container(
+                  color: Colors.grey,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(child: Text("Pilih Kategori")),
+                      IconButton(
+                          padding: EdgeInsets.zero,
+                          onPressed: () => Navigator.of(context).pop(),
+                          icon: Icon(Icons.close))
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  Widget _listtile({required VoidCallback onTap}) {
     return ListTile(
       shape: RoundedRectangleBorder(
           side: BorderSide(), borderRadius: BorderRadius.circular(6)),
-      onTap: () {},
+      onTap: onTap,
       title: Text("Makanan"),
       leading: Icon(Icons.add),
       trailing: CircleAvatar(
